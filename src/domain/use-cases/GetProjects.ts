a6bfc9825e37df64b6ed3d417208d612d16682c1
Adrián Fúnez <adriianh@proton.me>
2026-06-12 00:43:06 -0600

    refactor(domain): update GetProjects use case to evaluate business rules via specifications

--- src/domain/use-cases/GetProjects.ts
+++ src/domain/use-cases/GetProjects.ts
@@ -1,12 +1,16 @@
 import { Project } from "../entities/Project";
+import { AllProjectsSpec } from "../specs/ProjectSpecs";
 import type { ProjectRepository } from "../repositories/ProjectRepository";
+import type { Specification } from "../specs/Specification";
 
 export class GetProjects {
     constructor(private readonly projectRepository: ProjectRepository) {}
 
-    async execute(): Promise<Project[]> {
+    async execute(
+        spec: Specification<Project> = new AllProjectsSpec(),
+    ): Promise<Project[]> {
         const projects = await this.projectRepository.getProjects();
 
-        return projects.sort((a, b) => a.title.localeCompare(b.title));
+        return projects.filter((project) => spec.isSatisfiedBy(project));
     }
 }
